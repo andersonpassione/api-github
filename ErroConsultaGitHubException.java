951477f470649e287c63e4386b3e9a024e179b39
Anderson Passione <andersonpassione@gmail.com>
2024-07-06 15:34:00 -0300

    Modificando a exception, para na construção passar o parametro direto para a classe super

--- ErroConsultaGitHubException.java
+++ ErroConsultaGitHubException.java
@@ -1,14 +1,8 @@
 package br.com.alura.screenmatch.exercicioApiGitHub;
 
 public class ErroConsultaGitHubException extends RuntimeException{
-    private String mensagem;
 
     public ErroConsultaGitHubException(String mensagem) {
-        this.mensagem = mensagem;
-    }
-
-    @Override
-    public String getMessage() {
-        return this.mensagem;
+        super(mensagem);
     }
 }
